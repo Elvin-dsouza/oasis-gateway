d0124ffd1f1525beb4a0a09a7793546f5ff8dc00
Elvin-dsouza <sdsouza3011@gmail.com>
2021-07-17 22:45:52 +0530

    save to database

--- routes/capture.js
+++ routes/capture.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var router = express.Router();
-const { Kafka } = require('kafkajs')
+const { Kafka } = require('kafkajs');
+var loki = require('lokijs');
+var db = new loki('Example');
 const kafka = new Kafka({
   clientId: 'oasis-sensor-log',
   brokers: [`acesd.online:29092`]
 });
 
 let lastLog = 0;
-
+var log = db.addCollection('sensorLog', { indices: ['timestamp'] });
 const logSensorInformation = async (pm25, pm10, pm1, temperature, humidity) => {
   const producer = kafka.producer()
   await producer.connect();
   if(!lastLog) {
     lastLog = Date.now();
+    log.insert({ 
+      pm25,
+      pm10,
+      pm1,
+      temperature,
+      humidity,
+      timestamp: Date.now()
+    });
+    
     await producer.send({
       topic: 'sensor-output-log-hist',
       messages: [
